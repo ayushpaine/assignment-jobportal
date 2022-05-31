8e2ca5fbd17df381440295557e5449ab95427230
ayushpaine <ayushmt701@gmail.com>
2022-05-31 19:51:12 +0530

    modal with basic details section created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import Box from "@mui/material/Box";
 import theme from "./themes/theme";
 import "./App.css";
 import { ThemeProvider } from "@mui/material";
 import Header from "./components/Header";
-import { Grid, Paper, Button, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import SearchTag from "./components/SearchTag";
+import ModalJob from "./components/ModalJob";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <ModalJob />
       <Grid container justify="center ">
         <Grid item xs={10}>
           <SearchTag />
